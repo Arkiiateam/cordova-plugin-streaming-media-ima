248e87e91b9525f26b62c924e55bc36b757136da
Alouane Nour-Eddine <alouane00@gmail.com>
2018-07-14 18:05:14 +0100

    Update mediaima java file

--- src/android/StreamingMediaIMA.java
+++ src/android/StreamingMediaIMA.java
@@ -1,4 +1,7 @@
-package cordova.plugin.streamingmediaima;
+package cordova.plugin.streamingmediaima.StreamingMediaIMA;
+
+import android.content.Context;
+import com.ionicframework.elbotola609035.R;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -7,78 +10,59 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import es.lombrinus.projects.mods.AJCPlayer;
+import cn.jzvd.JZVideoPlayerStandard;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class StreamingMediaIMA extends CordovaPlugin {
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
-        }
-        else if(action.equals("playVideo")) {
+  private Context context;
 
-            this.callbackContext = callbackContext;
-		    JSONObject options = null;
-            try {
-                options = args.getJSONObject(1);
-            } catch (JSONException e) {
-                // Developer provided no options. Leave options null.
-            }
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if (action.equals("coolMethod")) {
+      String message = args.getString(0);
+      this.coolMethod(message, callbackContext);
+      return true;
+    }
+    else if(action.equals("playVideo")) {
 
-            this.playVideo(args.getString(0), options);
+      this.context = this.cordova.getActivity().getApplicationContext();
 
-            return true;
+      JSONObject options = null;
+      try {
+        options = args.getJSONObject(1);
+      } catch (JSONException e) {
+        // Developer provided no options. Leave options null.
+      }
 
-        }
+      this.playVideo(args.getString(0), options);
 
-        return false;
+      return true;
+
     }
 
-    private boolean playVideo(String url, JSONObject options) {
+    return false;
+  }
 
-		// return play(SimpleVideoStream.class, url, options);
-        AJCPlayer videoPlayer = new VideoPlayer(this, new MediaPlayer());
+  private boolean playVideo(String url, JSONObject options) {
 
-        // load SurfaceHolder
-        SurfaceHolder holder = mSurfaceView.getHolder();
-        DisplayMetrics metrics = new DisplayMetrics();
-        this.getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        holder.setFixedSize(metrics.widthPixels, (int) ((float) metrics.widthPixels / (float) 16 / (float) 9));
-        holder.addCallback(this);
+    JZVideoPlayerStandard jzVideoPlayerStandard = (JZVideoPlayerStandard) cordova.getActivity().findViewById(R.id.videoplayer);
+    jzVideoPlayerStandard.setUp("http://jzvd.nathen.cn/c6e3dc12a1154626b3476d9bf3bd7266/6b56c5f0dc31428083757a45764763b0-5287d2089db37e62345123a1be272f8b.mp4",
+      JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL,
+      "饺子闭眼睛");
+//    jzVideoPlayerStandard.thumbImageView.setImageURI("http://p.qpic.cn/videoyun/0/2449_43b6f696980311e59ed467f22794e792_1/640");
 
-        // views you want to hide automatically when content is playing
-        View viewBot = findViewById(R.id.audioPlayerLayoutBottom);
-        View viewTop = findViewById(R.id.audioPlayerLayoutTop);
-        CList viewsToHide = new CList(viewBot, viewTop);
-
-        // we send FrameLayout as param because AJCPlayer uses this view to detect click and double click
-        FrameLayout mFrameLayout = (FrameLayout) findViewById(R.id.videoSurfaceContainer);
-                
-        final VideoPlayerView videoPlayerView = new VideoPlayerView(mFrameLayout, surfaceHolder, currentPositionTextView, durationTextView, seekBar, viewsToHide); // you could send currentPosition, duration or seekbar views as null
-
-        final VideoPlayerOptions options = new VideoPlayerOptions(ActivityInfo.SCREEN_ORIENTATION_SENSOR, true, true, true);
-        final Controls controls = new Controls(plays, pauses, stops);
-        controlBarManager = new VideoControlBarManager(context, controls, new LoadingView(){...}, new OnDoubleClick{...}, videoPlayerView, options); //LoadingView you can hide/show your progress bar. LoadingView and OnDoubleClick could be null
-        videoPlayer.addEventListener(controlBarManager);
-
-        Asset asset = new Asset(idString, "http://cdnamd-hls-globecast.akamaized.net/live/ramdisk/arriadia/hls_snrt/index.m3u8", ContentType.VIDEO);
-        videoPlayer.play(asset, true); // autoplay=true
+      return true;
+  }
 
 
-	}
-
-
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+  private void coolMethod(String message, CallbackContext callbackContext) {
+    if (message != null && message.length() > 0) {
+      callbackContext.success(message);
+    } else {
+      callbackContext.error("Expected one non-empty string argument.");
     }
+  }
 }
