d82f3da3d9e50b5aa924eb6bd6145e70632faa2d
Alouane Nour-Eddine <alouane00@gmail.com>
2018-07-14 17:59:52 +0100

    Add res layout file

--- src/android/StreamingMediaIMA.java
+++ src/android/StreamingMediaIMA.java
@@ -7,6 +7,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import es.lombrinus.projects.mods.AJCPlayer;
+
 /**
  * This class echoes a string called from JavaScript.
  */
@@ -19,8 +21,58 @@ public class StreamingMediaIMA extends CordovaPlugin {
             this.coolMethod(message, callbackContext);
             return true;
         }
+        else if(action.equals("playVideo")) {
+
+            this.callbackContext = callbackContext;
+		    JSONObject options = null;
+            try {
+                options = args.getJSONObject(1);
+            } catch (JSONException e) {
+                // Developer provided no options. Leave options null.
+            }
+
+            this.playVideo(args.getString(0), options);
+
+            return true;
+
+        }
+
         return false;
     }
+
+    private boolean playVideo(String url, JSONObject options) {
+
+		// return play(SimpleVideoStream.class, url, options);
+        AJCPlayer videoPlayer = new VideoPlayer(this, new MediaPlayer());
+
+        // load SurfaceHolder
+        SurfaceHolder holder = mSurfaceView.getHolder();
+        DisplayMetrics metrics = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        holder.setFixedSize(metrics.widthPixels, (int) ((float) metrics.widthPixels / (float) 16 / (float) 9));
+        holder.addCallback(this);
+
+        // views you want to hide automatically when content is playing
+        View viewBot = findViewById(R.id.audioPlayerLayoutBottom);
+        View viewTop = findViewById(R.id.audioPlayerLayoutTop);
+        CList viewsToHide = new CList(viewBot, viewTop);
+
+        // we send FrameLayout as param because AJCPlayer uses this view to detect click and double click
+        FrameLayout mFrameLayout = (FrameLayout) findViewById(R.id.videoSurfaceContainer);
+                
+        final VideoPlayerView videoPlayerView = new VideoPlayerView(mFrameLayout, surfaceHolder, currentPositionTextView, durationTextView, seekBar, viewsToHide); // you could send currentPosition, duration or seekbar views as null
+
+        final VideoPlayerOptions options = new VideoPlayerOptions(ActivityInfo.SCREEN_ORIENTATION_SENSOR, true, true, true);
+        final Controls controls = new Controls(plays, pauses, stops);
+        controlBarManager = new VideoControlBarManager(context, controls, new LoadingView(){...}, new OnDoubleClick{...}, videoPlayerView, options); //LoadingView you can hide/show your progress bar. LoadingView and OnDoubleClick could be null
+        videoPlayer.addEventListener(controlBarManager);
+
+        Asset asset = new Asset(idString, "http://cdnamd-hls-globecast.akamaized.net/live/ramdisk/arriadia/hls_snrt/index.m3u8", ContentType.VIDEO);
+        videoPlayer.play(asset, true); // autoplay=true
+
+
+	}
+
 
     private void coolMethod(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
--- www/StreamingMediaIMA.js
+++ www/StreamingMediaIMA.js
@@ -3,3 +3,8 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'StreamingMediaIMA', 'coolMethod', [arg0]);
 };
+
+module.exports.playVideo = function (url, options) {
+	options = options || {};
+	cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMediaIMA", "playVideo", [url, options]);
+};
