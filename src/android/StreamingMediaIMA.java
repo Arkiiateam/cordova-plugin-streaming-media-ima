0e66c07d677057ceed74e2e8c21e5b9cacc0a4df
Alouane Nour-Eddine <alouane00@gmail.com>
2018-07-14 20:48:33 +0100

    Separate stremaing media activity

--- src/android/StreamingMediaIMA.java
+++ src/android/StreamingMediaIMA.java
@@ -1,8 +1,11 @@
-package cordova.plugin.streamingmediaima.StreamingMediaIMA;
+package cordova.plugins.streamingmediaima;
 
-import android.content.Context;
-import com.ionicframework.elbotola609035.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -10,59 +13,101 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import cn.jzvd.JZVideoPlayerStandard;
+import java.util.Iterator;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class StreamingMediaIMA extends CordovaPlugin {
 
-  private Context context;
+  public static final String ACTION_PLAY_AUDIO = "playAudio";
+  public static final String ACTION_PLAY_VIDEO = "playVideo";
+
+  private static final int ACTIVITY_CODE_PLAY_MEDIA = 7;
+
+  private CallbackContext callbackContext;
+
+  private static final String TAG = "StreamingMediaPlugin";
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    if (action.equals("coolMethod")) {
-      String message = args.getString(0);
-      this.coolMethod(message, callbackContext);
-      return true;
-    }
-    else if(action.equals("playVideo")) {
+    this.callbackContext = callbackContext;
+    JSONObject options = null;
 
-      this.context = this.cordova.getActivity().getApplicationContext();
-
-      JSONObject options = null;
-      try {
-        options = args.getJSONObject(1);
-      } catch (JSONException e) {
-        // Developer provided no options. Leave options null.
-      }
-
-      this.playVideo(args.getString(0), options);
-
-      return true;
-
+    try {
+      options = args.getJSONObject(1);
+    } catch (JSONException e) {
+      // Developer provided no options. Leave options null.
     }
 
-    return false;
-  }
-
-  private boolean playVideo(String url, JSONObject options) {
-
-    JZVideoPlayerStandard jzVideoPlayerStandard = (JZVideoPlayerStandard) cordova.getActivity().findViewById(R.id.videoplayer);
-    jzVideoPlayerStandard.setUp("http://jzvd.nathen.cn/c6e3dc12a1154626b3476d9bf3bd7266/6b56c5f0dc31428083757a45764763b0-5287d2089db37e62345123a1be272f8b.mp4",
-      JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL,
-      "饺子闭眼睛");
-//    jzVideoPlayerStandard.thumbImageView.setImageURI("http://p.qpic.cn/videoyun/0/2449_43b6f696980311e59ed467f22794e792_1/640");
-
-      return true;
-  }
-
-
-  private void coolMethod(String message, CallbackContext callbackContext) {
-    if (message != null && message.length() > 0) {
-      callbackContext.success(message);
+    if (ACTION_PLAY_AUDIO.equals(action)) {
+      return playAudio(args.getString(0), options);
+    } else if (ACTION_PLAY_VIDEO.equals(action)) {
+      return playVideo(args.getString(0), options);
     } else {
-      callbackContext.error("Expected one non-empty string argument.");
+      callbackContext.error("streamingMedia." + action + " is not a supported method.");
+      return false;
+    }
+  }
+
+  private boolean playAudio(String url, JSONObject options) {
+//    return play(StreamingAudio.class, url, options);
+    return true;
+  }
+  private boolean playVideo(String url, JSONObject options) {
+    url = "http://jzvd.nathen.cn/c6e3dc12a1154626b3476d9bf3bd7266/6b56c5f0dc31428083757a45764763b0-5287d2089db37e62345123a1be272f8b.mp4";
+    return play(StreamingVideo.class, url, options);
+  }
+
+  private boolean play(final Class activityClass, final String url, final JSONObject options) {
+    final CordovaInterface cordovaObj = cordova;
+    final CordovaPlugin plugin = this;
+
+    cordova.getActivity().runOnUiThread(new Runnable() {
+      public void run() {
+        final Intent streamIntent = new Intent(cordovaObj.getActivity().getApplicationContext(), activityClass);
+        Bundle extras = new Bundle();
+        extras.putString("mediaUrl", url);
+
+        if (options != null) {
+          Iterator<String> optKeys = options.keys();
+          while (optKeys.hasNext()) {
+            try {
+              final String optKey = (String)optKeys.next();
+              if (options.get(optKey).getClass().equals(String.class)) {
+                extras.putString(optKey, (String)options.get(optKey));
+                Log.v(TAG, "Added option: " + optKey + " -> " + String.valueOf(options.get(optKey)));
+              } else if (options.get(optKey).getClass().equals(Boolean.class)) {
+                extras.putBoolean(optKey, (Boolean)options.get(optKey));
+                Log.v(TAG, "Added option: " + optKey + " -> " + String.valueOf(options.get(optKey)));
+              }
+
+            } catch (JSONException e) {
+              Log.e(TAG, "JSONException while trying to read options. Skipping option.");
+            }
+          }
+          streamIntent.putExtras(extras);
+        }
+
+        cordovaObj.startActivityForResult(plugin, streamIntent, ACTIVITY_CODE_PLAY_MEDIA);
+      }
+    });
+    return true;
+  }
+
+  public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    Log.v(TAG, "onActivityResult: " + requestCode + " " + resultCode);
+    super.onActivityResult(requestCode, resultCode, intent);
+    if (ACTIVITY_CODE_PLAY_MEDIA == requestCode) {
+      if (Activity.RESULT_OK == resultCode) {
+        this.callbackContext.success();
+      } else if (Activity.RESULT_CANCELED == resultCode) {
+        String errMsg = "Error";
+        if (intent != null && intent.hasExtra("message")) {
+          errMsg = intent.getStringExtra("message");
+        }
+        this.callbackContext.error(errMsg);
+      }
     }
   }
 }
